94f98feb319915ca2272eae9a0e162e7af29424a
Lisa Chan <xlisachan@gmail.com>
2018-02-15 15:05:57 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -3,25 +3,26 @@ function callMe() {
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  return lyric;
   var lyric = "maybe";
+  return lyric;
+
 }
 
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
-
+  thisIsCrazy;
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
 }
+crazy();
 
 function sayMyName() {
   // fix the code in here:
-  var name = "Cricky";
-
   sayMy();
+
+  var name = "Cricky";
 
   function sayMy() {
     console.log(name);
